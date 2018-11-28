94d5b3315eebf7c09634503bab70cf944f6689df
Spencer Casteel <spencercasteel@gmail.com>
2018-11-28 09:30:51 -0600

    added class swift file

---
+++ 911Runs/utilities/StructureRunReportClass.swift
@@ -0,0 +1,9 @@
+//
+//  StructureRunReportClass.swift
+//  911Runs
+//
+//  Created by Spencer Casteel on 11/28/18.
+//  Copyright © 2018 Spencer Casteel. All rights reserved.
+//
+
+import Foundation
