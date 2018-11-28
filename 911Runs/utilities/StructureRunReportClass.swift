89a52ad10c4ece9e60fff2eb081643b99351b4d9
Spencer Casteel <spencercasteel@gmail.com>
2018-11-28 09:51:51 -0600

    added first page

--- 911Runs/utilities/StructureRunReportClass.swift
+++ 911Runs/utilities/StructureRunReportClass.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+class StructureRunReport {
+    var date: Date
+    var name: String
+    var address: String
+    var phoneNumber: String
+    var ownerName: String
+    var ownerAddress: String
+    var ownerPhoneNumber: String
+    var incidentNumber: String
+    var exposure: String
+    
+    init(date: Date, name: String, address: String, phoneNumber: String, ownerName: String, ownerAddress: String, ownerPhoneNumber: String, incidentNumber: String, exposure: String) {
+        self.date = date
+        self.name = name
+        self.address = address
+        self.phoneNumber = phoneNumber
+        self.ownerName = ownerName
+        self.ownerAddress = ownerAddress
+        self.ownerPhoneNumber = ownerPhoneNumber
+        self.incidentNumber = incidentNumber
+        self.exposure = exposure
+    }
+}
