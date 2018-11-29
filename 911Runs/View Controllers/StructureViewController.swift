24ae3d823b920836b88e4ad0ef0eabb37809ef6c
Spencer Casteel <spencercasteel@gmail.com>
2018-11-29 14:30:31 -0600

    added outlets

--- 911Runs/View Controllers/StructureViewController.swift
+++ 911Runs/View Controllers/StructureViewController.swift
@@ -10,12 +10,24 @@ import UIKit
 
 class StructureViewController: UIViewController {
     
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var incidentNumTextFeild: UITextField!
+    @IBOutlet weak var FDIDTextFeild: UITextField!
+    @IBOutlet weak var stateTextFeild: UITextField!
+    @IBOutlet weak var exposureTextFeild: UITextField!
+    @IBOutlet weak var alarmDatePicker: UIDatePicker!
+    @IBOutlet weak var arrivalDatePicker: UIDatePicker!
+    @IBOutlet weak var lastUnitDatePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
     }
  
+    
+    
+    
     /*
      // MARK: - Navigation
      
