7ae63fb5098a7f654d7c4da170156f97017945a3
Alex Richardson <richardson.d.alex@gmail.com>
2018-12-03 14:43:09 -0600

    Finished up to Location Page

--- 911Runs/View Controllers/Structure View Controllers/PersonnelViewController.swift
+++ 911Runs/View Controllers/Structure View Controllers/PersonnelViewController.swift
@@ -11,17 +11,41 @@ import UIKit
 class PersonnelViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return PersonnelManager.sharedInstance.getPersonnelListCount()
+        
+        if tableView == personnelTableView {
+            
+            return PersonnelManager.sharedInstance.getPersonnelListCount()
+            
+        } else {
+            
+            return ApparatusManager.sharedInstance.getApparatusListCount()
+            
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "personnelList") as! PersonnelTableViewCell
         
-        let currentName = PersonnelManager.sharedInstance.getPersonnelName(at: indexPath.row)
-        
-        cell.personnelName.text = currentName
-        
-        return cell
+        if tableView == personnelTableView {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "personnelList") as! PersonnelTableViewCell
+            
+            let currentName = PersonnelManager.sharedInstance.getPersonnelName(at: indexPath.row)
+            
+            cell.personnelName.text = currentName
+            
+            return cell
+            
+        } else {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "apparatusCell") as! ApparatusTableViewCell
+            
+            let currentApparatus = ApparatusManager.sharedInstance.getApparatusName(at: indexPath.row)
+            
+            cell.apparatusLabel.text = currentApparatus
+            
+            return cell
+            
+        }
     }
     
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
@@ -32,17 +56,18 @@ class PersonnelViewController: UIViewController, UITableViewDelegate, UITableVie
         return [deleteAction]
     }
     
-
+    
     @IBOutlet weak var nameOfPersonnel: UITextField!
     
     @IBOutlet weak var respondingApparatus: UITextField!
     
     @IBOutlet weak var personnelTableView: UITableView!
     
+    @IBOutlet weak var apparatusTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
     @IBAction func addPersonnelButtonTapped(_ sender: Any) {
@@ -55,25 +80,48 @@ class PersonnelViewController: UIViewController, UITableViewDelegate, UITableVie
         }
         
         PersonnelManager.sharedInstance.personnelListArray.append(personnelName)
-
-            print("\(PersonnelManager.sharedInstance.personnelListArray)")
         
-            personnelTableView.reloadData()
+        print("\(PersonnelManager.sharedInstance.personnelListArray)")
         
-            nameOfPersonnel.text = ""
+        personnelTableView.reloadData()
+        
+        nameOfPersonnel.text = ""
         
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addApparatusButtonTapped(_ sender: Any) {
+        
+        guard let apparatusName = respondingApparatus.text, apparatusName.trimmingCharacters(in: .whitespacesAndNewlines) != "" else {
+            
+            showErrorAlert(self, "Empty Fields", "Please enter in a value for all fields", "Close")
+            
+            return
+        }
+        
+        ApparatusManager.sharedInstance.apparatusListArray.append(apparatusName)
+        
+        apparatusTableView.reloadData()
+        
+        respondingApparatus.text = ""
+        
     }
-    */
-
+    
+    @IBAction func nextButtonTapped(_ sender: Any) {
+        
+        self.performSegue(withIdentifier: "ShowLocationInfo", sender: self)
+        
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
