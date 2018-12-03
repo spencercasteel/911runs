873a5edc712cca116305302d5f4fa79e3320c67d
Alex Richardson <richardson.d.alex@gmail.com>
2018-12-03 09:23:50 -0600

    Added 1st Segue

--- 911Runs/View Controllers/StructureViewController.swift
+++ 911Runs/View Controllers/StructureViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 
 class StructureViewController: UIViewController {
     
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var incidentNumTextFeild: UITextField!
-    @IBOutlet weak var FDIDTextFeild: UITextField!
-    @IBOutlet weak var stateTextFeild: UITextField!
+    @IBOutlet weak var incidentNumTextField: UITextField!
+    
+    @IBOutlet weak var FDIDTextField: UITextField!
+    
+    @IBOutlet weak var stateTextField: UITextField!
+    
     @IBOutlet weak var exposureTextFeild: UITextField!
+    
     @IBOutlet weak var alarmDatePicker: UIDatePicker!
+    
     @IBOutlet weak var arrivalDatePicker: UIDatePicker!
+    
     @IBOutlet weak var lastUnitDatePicker: UIDatePicker!
     
     override func viewDidLoad() {
@@ -26,41 +31,26 @@ class StructureViewController: UIViewController {
     }
     
     @IBAction func buttonTapped(_ sender: Any) {
-        guard let incidentNum = incidentNumTextFeild.text?.trimmingCharacters(in: .whitespacesAndNewlines) else {
+        
+        
+        guard let FDID = FDIDTextField.text, FDID.trimmingCharacters(in: .whitespacesAndNewlines) != "", let state = stateTextField.text, state.trimmingCharacters(in: .whitespacesAndNewlines) != "" else {
             
+            showErrorAlert(self, "Empty Fields", "Please enter in a value for all fields", "Close")
             return
         }
         
-        guard let FDIDNum = FDIDTextFeild.text?.trimmingCharacters(in: .whitespacesAndNewlines) else {
-            
-            return
-        }
-        
-        guard let state = stateTextFeild.text?.trimmingCharacters(in: .whitespacesAndNewlines) else {
-            
-            return
-        }
-        
-        guard let exposure = exposureTextFeild.text?.trimmingCharacters(in: .whitespacesAndNewlines) else {
-            
-            return
-        }
-        
-        if incidentNum.isEmpty || FDIDNum.isEmpty || state.isEmpty || exposure.isEmpty {
-            return showErrorAlert(self, "Empty Fields", "Please enter in a value for all fields", "Close")
-        }
+        performSegue(withIdentifier: "ShowPersonnel", sender: self)
     }
-    
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
+
+
+
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
