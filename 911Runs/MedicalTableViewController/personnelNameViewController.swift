3b9881d54bf919f51ceeac881ba6f1c21aa391c8
Spencer Casteel <spencercasteel@gmail.com>
2018-12-03 10:49:21 -0600

    fixed all errors

--- 911Runs/MedicalTableViewController/personnelNameViewController.swift
+++ 911Runs/MedicalTableViewController/personnelNameViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class personnelNameTableViewController: UITableViewCell{
+class personnelNameTableViewController: UITableViewCell {
+
     @IBOutlet weak var personnelName: UILabel!
     
     override func awakeFromNib() {
