c4f8d0d2f4f1fd131e85b7dcc5fd1a45984d9fe0
Spencer Casteel <spencercasteel@gmail.com>
2018-12-04 19:54:01 -0600

    fixed all segues

--- 911Runs/View Controllers/Medical View Controllers/PatientsViewController.swift
+++ 911Runs/View Controllers/Medical View Controllers/PatientsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class PatientsViewController: UIViewController {
-
+    
     @IBOutlet weak var providerAssessmentPicker: UIPickerView!
     
     var providerAssessmentPickerData: [String] = [String]()
@@ -18,19 +18,21 @@ class PatientsViewController: UIViewController {
         super.viewDidLoad()
         
         providerAssessmentPickerData = ["10 Abdominal Pain", "11 Airway Obstrution","12 Allergic Reation", "13 Altered LOC", "14 Behavioral/Psych", "15 Burns", "16 Cardiac Arrest", "18 Chest Pain", "19 Diabetic Symptom", "20 Do Not Resuscitate", "21 Electrocution", "22 General Illness", "23 Hemorrhaging/Bleeding", "24 Hyperthermia", "25 Hypothermia", "26 Hypovolemia", "27 Inhalation Injury", "28 Obvious Death", "29 OD/Poisoning", "30 Pregnancy/OB", "31 Pespiratory Arrest", "32 Pespiratory Distress", "33 Seizures", "34 Sexual Assault", "Sting/Bite", "36 Stroke/CVA", "37 Syncope", "38 Trauma", "OO Other", "NN None/No Patient"]
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func nextButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "patientsToInjury", sender: self)
     }
-    */
-
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
