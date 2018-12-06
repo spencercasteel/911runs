e4dc4177311dfec3e500fa719d8c0e181ef1f1f6
Spencer Casteel <spencercasteel@gmail.com>
2018-12-06 10:12:31 -0600

    changed for ui changes

--- 911Runs/View Controllers/Medical View Controllers/LocationViewController.swift
+++ 911Runs/View Controllers/Medical View Controllers/LocationViewController.swift
@@ -17,18 +17,27 @@ class LocationViewController: UIViewController {
     @IBOutlet weak var ownerNameTextField: UITextField!
     @IBOutlet weak var ownerAddressTextField: UITextField!
     @IBOutlet weak var ownerPhoneTextField: UITextField!
+    @IBOutlet weak var ownerCityTextField: UITextField!
+    @IBOutlet weak var ownerStateTextField: UITextField!
+    @IBOutlet weak var ownerZipTextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var zipTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         ownerNameTextField.isHidden = true
         ownerAddressTextField.isHidden = true
         ownerPhoneTextField.isHidden = true
+        ownerCityTextField.isHidden = true
+        ownerStateTextField.isHidden = true
+        ownerZipTextField.isHidden = true
         
         // Do any additional setup after loading the view.
     }
     
     @IBAction func nextButtonTyped(_ sender: Any) {
-        guard let name = nameTextField.text, name.trimmingCharacters(in: .whitespacesAndNewlines) != "", let address = addressTextField.text, address.trimmingCharacters(in: .whitespacesAndNewlines) != "", let phone = phoneTextField.text, phone.trimmingCharacters(in: .whitespacesAndNewlines) != "" else {
+        guard let name = nameTextField.text, name.trimmingCharacters(in: .whitespacesAndNewlines) != "", let address = addressTextField.text, address.trimmingCharacters(in: .whitespacesAndNewlines) != "", let phone = phoneTextField.text, phone.trimmingCharacters(in: .whitespacesAndNewlines) != "", let city = cityTextField.text, city.trimmingCharacters(in: .whitespacesAndNewlines) != "", let state = stateTextField.text, state.trimmingCharacters(in: .whitespacesAndNewlines) != "", let zip = zipTextField.text, zip.trimmingCharacters(in: .whitespacesAndNewlines) != "" else {
             
             
             showErrorAlert(self, "Empty Fields", "Please enter in a value for all fields", "Close")
@@ -36,7 +45,7 @@ class LocationViewController: UIViewController {
             
         }
         if ownerInfoSegmentedControl.selectedSegmentIndex == 1 {
-            guard let ownerName = ownerNameTextField.text, ownerName.trimmingCharacters(in: .whitespacesAndNewlines) != "", let ownerAddress = ownerAddressTextField.text, ownerAddress.trimmingCharacters(in: .whitespacesAndNewlines) != "", let ownerPhone = ownerPhoneTextField.text, ownerPhone.trimmingCharacters(in: .whitespacesAndNewlines) != "" else {
+            guard let ownerName = ownerNameTextField.text, ownerName.trimmingCharacters(in: .whitespacesAndNewlines) != "", let ownerAddress = ownerAddressTextField.text, ownerAddress.trimmingCharacters(in: .whitespacesAndNewlines) != "", let ownerPhone = ownerPhoneTextField.text, ownerPhone.trimmingCharacters(in: .whitespacesAndNewlines) != "", let ownerCity = ownerCityTextField.text, ownerCity.trimmingCharacters(in: .whitespacesAndNewlines) != "", let ownerState = ownerStateTextField.text, ownerState.trimmingCharacters(in: .whitespacesAndNewlines) != "", let ownerZip = ownerZipTextField.text, ownerZip.trimmingCharacters(in: .whitespacesAndNewlines) != "" else {
                 
                 showErrorAlert(self, "Empty Fields", "Please enter in a value for all fields", "Close")
                 return
@@ -55,10 +64,16 @@ class LocationViewController: UIViewController {
             ownerNameTextField.isHidden = true
             ownerAddressTextField.isHidden = true
             ownerPhoneTextField.isHidden = true
+            ownerCityTextField.isHidden = true
+            ownerStateTextField.isHidden = true
+            ownerZipTextField.isHidden = true
         } else {
             ownerNameTextField.isHidden = false
             ownerAddressTextField.isHidden = false
             ownerPhoneTextField.isHidden = false
+            ownerCityTextField.isHidden = false
+            ownerStateTextField.isHidden = false
+            ownerZipTextField.isHidden = false
             
         }
     }
--- 911Runs/View Controllers/Medical View Controllers/medicPersonnelViewController.swift
+++ 911Runs/View Controllers/Medical View Controllers/medicPersonnelViewController.swift
@@ -33,30 +33,11 @@ class medicPersonnelViewController: UIViewController, UITableViewDataSource, UIT
         personnelNameTextField.text = ""
     }
     
-    @IBAction func addMedicButtonTapped(_ sender: Any) {
-        guard let medicName = medicNameTextField.text, medicName.trimmingCharacters(in: .whitespacesAndNewlines) != "", let unitNum = medicUnitNum.text, unitNum.trimmingCharacters(in: .whitespacesAndNewlines) != "" else {
-            
-            showErrorAlert(self, "Empty Fields", "Please enter in a value for all fields", "Close")
-            return
-        }
-        let combo = "\(medicName) | Medic ID: \(unitNum)"
-        MedicalManager.sharedInstance.medicArray.append(combo)
-        medicTableView.reloadData()
-        medicNameTextField.text = ""
-        medicUnitNum.text = ""
-    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if tableView == personnelTableView {
             return MedicalManager.sharedInstance.getPersonnelListCount()
-        } else {
-            
-            return MedicalManager.sharedInstance.getMedArrayCount()
         }
-    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if tableView == personnelTableView {
             let cell = tableView.dequeueReusableCell(withIdentifier: "personnelList") as! personnelNameTableViewController
             
             let currentName = MedicalManager.sharedInstance.getPersonnelName(at: indexPath.row)
@@ -65,40 +46,24 @@ class medicPersonnelViewController: UIViewController, UITableViewDataSource, UIT
             
             return cell
             
-        } else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "medicNameAndUnitNum") as! personnelNameTableViewController
-            
-            let currentMed = MedicalManager.sharedInstance.getMedInfo(at: indexPath.row)
-            
-            cell.medicNameAndUnitNum.text = currentMed
-            
-            return cell
         }
-    }
     
     
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-        if tableView == personnelTableView {
             let deleteAction = UITableViewRowAction(style: .destructive, title: "Delete") {_, _ in
                 MedicalManager.sharedInstance.deletePersonnelName(at: indexPath.row)
                 tableView.deleteRows(at: [indexPath], with: .fade)
             }
             return [deleteAction]
-        } else {
-            let deleteAction = UITableViewRowAction(style: .destructive, title: "Delete") {_, _ in
-                MedicalManager.sharedInstance.deleteMedicInfo(at: indexPath.row)
-                tableView.deleteRows(at: [indexPath], with: .fade)
-            }
-            return[deleteAction]
         }
-    }
+    
     
     
     @IBAction func nextPageButtonTapped(_ sender: Any) {
         if MedicalManager.sharedInstance.getPersonnelListCount() == 0 {
             showErrorAlert(self, "Empty Personnel", "Please enter the personnel that went on the run", "Close")
         } else {
-            performSegue(withIdentifier: "personnelToProperty", sender: self)
+            performSegue(withIdentifier: "personnelToApparatus", sender: self)
         }
     }
     /*
