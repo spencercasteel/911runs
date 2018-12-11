3ebe41985283fe8c1372feaaeabf7e548c8e146f
Spencer Casteel <spencercasteel@gmail.com>
2018-12-11 09:02:35 -0600

    fixed pickers -Spencer

--- 911Runs/View Controllers/Structure View Controllers/FireDetailsViewController.swift
+++ 911Runs/View Controllers/Structure View Controllers/FireDetailsViewController.swift
@@ -60,6 +60,38 @@ class FireDetailsViewController: UIViewController, UIPickerViewDelegate, UIPicke
         
     }
     
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == HeatSourcePicker {
+            let selectedRow = row
+            if row != 0 && HeatSourcePickerData[0] == "" {
+                HeatSourcePickerData.remove(at: 0)
+                pickerView.reloadComponent(component)
+                pickerView.selectRow(selectedRow - 1, inComponent: 0, animated: false)
+            }
+        } else if pickerView == FirstIgnitedPicker {
+            let selectedRow = row
+            if row != 0 && FirstIgnitedPickerData[0] == "" {
+                FirstIgnitedPickerData.remove(at: 0)
+                pickerView.reloadComponent(component)
+                pickerView.selectRow(selectedRow - 1, inComponent: 0, animated: false)
+            }
+        } else if pickerView == MaterialIgnitedPicker {
+            let selectedRow = row
+            if row != 0 && MaterialIgnitedPickerData[0] == "" {
+                MaterialIgnitedPickerData.remove(at: 0)
+                pickerView.reloadComponent(component)
+                pickerView.selectRow(selectedRow - 1, inComponent: 0, animated: false)
+            }
+        } else {
+            let selectedRow = row
+            if row != 0 && IgnitionPickerData[0] == "" {
+                IgnitionPickerData.remove(at: 0)
+                pickerView.reloadComponent(component)
+                pickerView.selectRow(selectedRow - 1, inComponent: 0, animated: false)
+            }
+        }
+    }
+    
     
     @IBOutlet weak var HeatSourcePicker: UIPickerView!
     
@@ -81,13 +113,13 @@ class FireDetailsViewController: UIViewController, UIPickerViewDelegate, UIPicke
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        HeatSourcePickerData = ["13 Arching", "43 Hot Embers", "61 Cigarette", "63 Und Smoking Mat", "65 Cigarette Lighter", "73 Lightning", "OO Other", "UU Udeterminded"]
+        HeatSourcePickerData = ["", "13 Arching", "43 Hot Embers", "61 Cigarette", "63 Und Smoking Mat", "65 Cigarette Lighter", "73 Lightning", "OO Other", "UU Udeterminded"]
         
-        FirstIgnitedPickerData = ["10 Structure Component", "17 Structure Frame", "20 Furniture", "30 Clothes", "60 Liquids", "81 Electrical Wire", "82 Transformer", "99 Multiple Items", "UU Undetermined"]
+        FirstIgnitedPickerData = ["", "10 Structure Component", "17 Structure Frame", "20 Furniture", "30 Clothes", "60 Liquids", "81 Electrical Wire", "82 Transformer", "99 Multiple Items", "UU Undetermined"]
         
-        MaterialIgnitedPickerData = ["12 LP Gas", "23 Gasoline", "25 Kerosene", "27 Cooking Oil", "41 Plastic", "60 Wood/Paper", "71 Fabric", "OO Other"]
+        MaterialIgnitedPickerData = ["", "12 LP Gas", "23 Gasoline", "25 Kerosene", "27 Cooking Oil", "41 Plastic", "60 Wood/Paper", "71 Fabric", "OO Other"]
         
-        IgnitionPickerData = ["1 Intentional", "2 Unintentional", "3 Equipment Failure", "4 Act of Nature", "5 Under Investigation", "U Undetermined"]
+        IgnitionPickerData = ["", "1 Intentional", "2 Unintentional", "3 Equipment Failure", "4 Act of Nature", "5 Under Investigation", "U Undetermined"]
         
 
         // Do any additional setup after loading the view.
